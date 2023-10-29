9b5d27c04cd359e07815434a4d429a6839ef765e
VitalyiNahatskyi <vnahatskyi2020@itcollege.lviv.ua>
2023-10-29 22:43:57 +0200

    completion of work in the second lab

---
+++ 2_lab/chatgpt.py
@@ -0,0 +1,35 @@
+# Змінні та Типи Даних
+x = 5       # Ціле число
+y = 2.7     # Число з плаваючою комою
+z = 1 + 3j  # Комплексне число
+
+text = "Привіт, світ!"
+char = 'a'   # Рядок одного символу
+
+is_valid = True
+is_invalid = False
+
+# Умовні оператори
+x = 10
+
+if x > 5:
+    print("x більше за 5")
+elif x == 5:
+    print("x дорівнює 5")
+else:
+    print("x менше 5")
+
+# Цикли
+for i in range(5):
+    print(i)
+
+counter = 0
+while counter < 5:
+    print(counter)
+    counter += 1
+
+# Функції
+def привіт_світ():
+    print("Привіт, світ!")
+
+привіт_світ()
